0d42426718dc08a1814d1b6a377cda039ba516c7
Matheus Nogueira Albuquerque <127986753+Nogz04@users.noreply.github.com>
2026-04-20 13:53:50 -0300

    Add comments to constants in valores_e_recargas.py

--- constantes_enums/valores_e_recargas.py
+++ constantes_enums/valores_e_recargas.py
@@ -6,6 +6,6 @@ class ValoresErecargas(Enum):
     Classe que armazena as constantes de valores de recarga mínimos, bônus e gatilho de bônus.
     """
 
-    VALOR_EM_REAIS_MINIMO = Decimal("20.00")
-    VALOR_GB_BONUS = int(20)
-    VALOR_EM_REAIS_GATILHO_BONUS = Decimal("50.00")
+    VALOR_EM_REAIS_MINIMO = Decimal("20.00") # Valor em R$ de recarga mínima.
+    VALOR_GB_BONUS = int(20) # Valor de bônus em GB quando elegível.
+    VALOR_EM_REAIS_GATILHO_BONUS = Decimal("50.00") # Valor em R$ de gatilho do bônus, quando atingir esse valor ou >, ativa o bônus.
